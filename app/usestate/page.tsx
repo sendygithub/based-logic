afd5998b21ad1ad1d74404e9fd141deff3255bb9
sendygithub <sendy.lazada@gmail.com>
2026-01-27 21:58:02 +0700

    routes

--- app/usestate/page.tsx
+++ app/usestate/page.tsx
@@ -7,8 +7,8 @@ export default function Page() {
     const [hasil, setHasil] = useState(0);
 
 
-    const X =()=>{
-        setHasil(Number(ongko))
+    const fungsiX =()=>{
+        setHasil(ongko*6)
     }
 
 
@@ -20,13 +20,13 @@ export default function Page() {
             <input
             type='text'
             value={ongko}
-            onChange={(e)=> rubahAngka(e.target.value)}
+            onChange={(e)=> rubahAngka(e.target.value)}//rubah isi angka
             className='bg-gray-100 hover:bg-gray-200'
             placeholder='input angka'
             />
 
 
-            <button onClick={X} className='bg-red-500 rounded-full'>ubah angka</button>
+            <button onClick={fungsiX} className='bg-red-500 rounded-full'>ubah angka</button>
             <p>hasil :{hasil}</p>
             
         </div>
